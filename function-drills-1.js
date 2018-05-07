ada1a43348a19e63a592efb8e40f572a25e13392
Steven Imus <imussg@gmail.com>
2018-05-07 15:21:51 -0400

    6

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,11 +1,20 @@
 'use strict';
 
 function yearOfBirth(age) {
-  return 2018 - age; 
+	try{
+		if(age < 0) {
+			throw new Error("Age can not be negative");
+		} else {
+			return 2018 - age;
+		}
+	}
+	catch (e) {
+		console.error(e.message);
+	}
 }
 function whoAmI(name, age) {
 	console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
 	console.log(`I was born in ${yearOfBirth(age)}`);
 }
 
-whoAmI('Sonya', 30);
+whoAmI('Sonya', -5);
