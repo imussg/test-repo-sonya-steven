9eb652a2275edb8e9a8af6d610ce4b4ab208ba27
Steven Imus <imussg@gmail.com>
2018-05-07 17:36:55 -0400

    beyond function

--- jediName.js
+++ jediName.js
@@ -3,4 +3,21 @@
 function jediName(firstName, lastName) {
   return `${lastName.slice(0, 3)}${firstName.slice(0, 2)}`;
 }
+
+function beyond(num) {
+	let returned = "";
+	if(num === Infinity) {
+		returned = "And beyond";
+	} else if(num > 0) {
+		returned = "To infinity";
+	} else if(num < 0) {
+		returned = "To negative infinity";
+	} else {
+		returned = "Staying home";
+	}
+	return returned;
+}
+
 console.log(jediName('Sonya', 'Slegers'));
+console.log(beyond(Math.pow(10, 1000)));
+console.log(beyond(100));
