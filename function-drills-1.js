ec0243b879b504f47645d49d245bbbc46b623f5f
Steven Imus <imussg@gmail.com>
2018-05-07 15:04:46 -0400

    4

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,5 +1,7 @@
 'use strict';
 function whoAmI(name, age) {
+	const yearOfBirth = 2018 - age;
 	console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+	console.log(`I was born in ${yearOfBirth}`);
 }
 whoAmI('Sonya', 30);
