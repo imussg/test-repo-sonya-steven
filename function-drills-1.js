d93abf79c655c5e7691093c5f1e8efd9e9973730
Sonya Slegers <sonjay08@gmail.com>
2018-05-07 15:18:33 -0500

    7

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,20 +1,24 @@
 'use strict';
 
 function yearOfBirth(age) {
-	try{
-		if(age < 0) {
-			throw new Error("Age can not be negative");
-		} else {
-			return 2018 - age;
-		}
-	}
-	catch (e) {
-		console.error(e.message);
-	}
+  try{
+    if(age < 0) {
+      throw new Error('Age can not be negative');
+    } else {
+      return 2018 - age;
+    }
+  }
+  catch (e) {
+    console.error(e.message);
+  }
 }
 function whoAmI(name, age) {
-	console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-	console.log(`I was born in ${yearOfBirth(age)}`);
+  if (name && age) { 
+    console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+    console.log(`I was born in ${yearOfBirth(age)}`);
+  } else {
+    console.error('Argument not valid');
+  }
 }
 
-whoAmI('Sonya', -5);
+whoAmI('Harry', 10);
