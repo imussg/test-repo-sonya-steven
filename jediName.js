0f215cd6cb5240a7ce53495900974da2ba20080f
Sonya Slegers <sonjay08@gmail.com>
2018-05-07 22:32:36 -0500

    last 3 drills

--- jediName.js
+++ jediName.js
@@ -46,32 +46,51 @@
 // console.log(beyond(Math.pow(10, 1000)));
 // console.log(beyond(100));
 
-function howManyDays(month, leapYear) {
-  if (leapYear === true && month === 'February') {
-    return 'It\'s a leap year so February has 29 days.';
-  } else {
-    switch(month) {
-    case 'January':
-    case 'March':
-    case 'May':
-    case 'July':
-    case 'August':
-    case 'October':
-    case 'December':
-      return `${month} has 31 days.`;
-      break;
-    case 'April':
-    case 'June':
-    case ' September':
-    case 'November':
-      return `${month} has 30 days.`;
-      break;
-    case 'February':
-      return `It's not a leap year so ${month} has 28 days.`;
-      break;
-    default:
-		  return 'Must provide a valid month';
-    }
-  }
+// function howManyDays(month, leapYear) {
+//   if (leapYear === true && month === 'February') {
+//     return 'It\'s a leap year so February has 29 days.';
+//   } else {
+//     switch(month) {
+//     case 'January':
+//     case 'March':
+//     case 'May':
+//     case 'July':
+//     case 'August':
+//     case 'October':
+//     case 'December':
+//       return `${month} has 31 days.`;
+//       break;
+//     case 'April':
+//     case 'June':
+//     case ' September':
+//     case 'November':
+//       return `${month} has 30 days.`;
+//       break;
+//     case 'February':
+//       return `It's not a leap year so ${month} has 28 days.`;
+//       break;
+//     default:
+// 		  return 'Must provide a valid month';
+//     }
+//   }
+// }
+// console.log(howManyDays(23, true));
+
+function rockPaperScissors(num) {
+	const randomNo = Math.floor(Math.random() * 3) + 1;
+  if (num === 1 && randomNo === 3 || 
+			num === 3 && randomNo === 2 || 
+			num === 2 && randomNo === 2) {
+    return 'Congratualtions!  You beat the computer!';
+  } else if (num === 1 && randomNo === 2 ||
+						 num === 2 && randomNo === 3 ||
+						 num === 3 && randomNo === 1) {
+									 return 'Bummer! The computer beat you...';
+								 } else if (num === randomNo) {
+									 return `It's a tie!`;
+								 } else {
+									 console.error(`Parameter is not a number`);
+								 }
 }
-console.log(howManyDays(23, true));
+
+console.log(rockPaperScissors(10));
